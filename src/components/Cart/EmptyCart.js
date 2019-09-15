190c17becd6726237c489dc64373e5e502fcd4c1
Adam <adamsamuelcoots@gmail.com>
2019-09-15 12:31:45 -0400

    Sizing update

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -4,7 +4,7 @@ export default function EmptyCart() {
   return (
     <div>
       <div className="row mt-5">
-        <div className="col-10 mx-auto text-center">
+        <div className="col-10 mx-auto text-center cart dark-mode-title">
           <h1>Cart is empty</h1>
         </div>
       </div>
